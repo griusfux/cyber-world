255681bbf6861c281d625eed1df740973f9acd0c
zDemoniac <zDemoniac@gmail.com>
2013-07-22 13:23:51 +0000

    - ai fixes - less logs

--- js/bullets.js
+++ js/bullets.js
@@ -1,9 +1,12 @@
-function  Bullet()
+function  Bullet(color)
 {
 	this.goal = new THREE.Vector3();
 	this.dx = new THREE.Vector3();
-	this.color = 0x550000;
+	this.color = color;
 	this.speed = 20;
+	this.damageMin = 10;
+	this.damageMax = 20;
+	this.target = null;
 
 	var that = this;
     this.onGeometry = function(geom, mats) {
@@ -17,23 +20,30 @@ function  Bullet()
 //    loader.load( "models/unit0.js", onGeometry );    
     this.onGeometry(new THREE.CubeGeometry( .1, .1, .4 ), null);
 
-	this.fire = function(position, goal) {
+	this.fire = function(position, target) {
+		if (this.mesh.visible) return; // allready fire
+
 		this.mesh.visible = true;
 		this.mesh.position = position.clone();
-		this.goal = goal;
+		this.target = target;
 	};
 
 	this.update = function(dt) {
         if (!this.mesh || !this.mesh.visible) return;
+		var goal = this.target.mesh.position;
 
-        this.mesh.lookAt(this.goal);
-        this.dx.subVectors(this.goal, this.mesh.position);
-		if (this.dx.length() > .01) {
+        this.mesh.lookAt(goal);
+        this.dx.subVectors(goal, this.mesh.position);
+		if (this.dx.length() > .1) {
 			var moveDist = dt * this.speed;
 			this.mesh.translateZ(moveDist);
         }
 		else {
 			this.mesh.visible = false;
+			// do damage
+			var dmg = Math.random()*(this.damageMax - this.damageMin) + this.damageMin;
+			this.target.health -= dmg;
+			//log("damaged by: " + dmg);
         }
 	};
 }
